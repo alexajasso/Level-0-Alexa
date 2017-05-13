cb00e208b51f941588e68405feb4b165f092a239
League <Level 2>
2017-05-13 11:27:40 -0700

    I had fun and, #JAVA is awesome

---
+++ src/MorningZombie.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+System.out.println("Hi my name is juanita");
+JOptionPane.showMessageDialog(null,"Hi my name is Juanita");
+JOptionPane.showMessageDialog(null,"change your clothes");
+JOptionPane.showMessageDialog(null,"brush your hair");
+JOptionPane.showMessageDialog(null, "brush your teeth");
+JOptionPane.showMessageDialog(null,"eat breakfast");
+JOptionPane.showMessageDialog(null,"say good morning");
+}
+}
---
+++ src/knockknock.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+JOptionPane.showInputDialog("knock knock");
+JOptionPane.showInputDialog("Boo");
+JOptionPane.showInputDialog("Don't cry,its just me.");
+
+
+
+}
+}
