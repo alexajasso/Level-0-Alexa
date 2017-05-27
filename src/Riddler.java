19dad1d9688937343272f69b706fb11d3ad298b0
alexajasso <league@FunkyMini.local>
2017-05-27 11:26:00 -0700

    We did riddles that spoke to us.

---
+++ src/Riddler.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Riddler {
+	public static void main(String[] args) {
+	JOptionPane.showInputDialog("Mary's father has 5 daughters- Nana,Nene, Nini, Nono. What is the fifth daughters name?");
+	JOptionPane.showMessageDialog(null,"If you answered Nunu, you are wrong. It’s Mary!");
+	JOptionPane.showInputDialog("What do you get when you cross a pig and a tortoise");
+    JOptionPane.showMessageDialog(null,"a slow-pork" );
+    JOptionPane.showInputDialog("What do you call a famous turtle");
+    JOptionPane.showMessageDialog(null,"A shellebrity");
+    JOptionPane.showInputDialog("What do you call a turtle that poops a lot");
+    JOptionPane.showMessageDialog(null,"a turd le");
+    
+	}
+}
---
+++ src/Warmup.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Hello my name is Alexa");
+		JOptionPane.showMessageDialog(null,"I like turtles");
+		JOptionPane.showMessageDialog(null,"I think baby sea turtles are cute");
+		JOptionPane.showMessageDialog(null, " I love tacos");
+		JOptionPane.showMessageDialog(null, "I like Java");
+		JOptionPane.showMessageDialog(null, "I like to sleep");
+        JOptionPane.showMessageDialog(null, "I like my dogs");
+	}}
