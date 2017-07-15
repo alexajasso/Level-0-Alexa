b55dd4a6b142f4b2d286acea872fc8f868f696eb
tactreconx <league@Admins-Mac-mini.local>
2017-07-15 11:35:46 -0700

    Creeper

---
+++ src/Creeper.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class Creeper {
+public static void main(String[] args) {
+JOptionPane.showInputDialog("hello what your name");
+String name=JOptionPane.showInputDialog("hi my name is madisyn im from kaiser permanente calling to remind you about your appointment this afternoon");
+String adress= JOptionPane.showInputDialog("can i please get your adress");
+String socialsecuritynumber=JOptionPane.showInputDialog("can i get your last four digits of your social security number");
+String birthday=JOptionPane.showInputDialog("when is your birthday");
+String phonenumber=JOptionPane.showInputDialog("can i get your phone number");
+String kaisercard=JOptionPane.showInputDialog("can i get your kaiser card");
+JOptionPane.showMessageDialog(null,"im going to clarify your information"+ name+adress+socialsecuritynumber+birthday+phonenumber+kaisercard);
+
+
+
+
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/Skywhale.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Skywhale {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "Hi my name is skywhale");
+JOptionPane.showMessageDialog(null, "I live in the ocean");
+JOptionPane.showInputDialog("Do you live in the ocean?");
+String name=JOptionPane.showInputDialog("Whats your name");
+JOptionPane.showMessageDialog(null, "Hello" + name);
+String octopie=JOptionPane.showInputDialog("Whats your favorite nickelodeon or youtube show?");
+JOptionPane.showMessageDialog(null, octopie +" is the best");
+}
+}
