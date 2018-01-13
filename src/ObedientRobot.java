123915f7190b5750893362ea211aaad907ec40df
alexajasso <league@Mac-mini-4.local>
2018-01-13 11:26:59 -0800

    Still working on obedient robot.

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,17 +1,57 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
+	static Robot flash = new Robot();
 
 	public static void main(String[] args) {
-		Robot r2d2 = new Robot();
-	r2d2.penDown();    
-	//r2d2.setSpeed(200);
-	r2d2.move(90);
-	r2d2.turn(90);
-	
-	
-	
-	
+		drawSquare();
+		drawTriangle();
+		drawCircle();
 	}
 
+	private static void drawSquare() {
+		flash.setPenColor(255, 0, 0);
+		flash.penDown();
+		flash.setSpeed(2000000000);
+			flash.move(90);
+		    flash.turn(90);
+		    flash.move(90);
+		    flash.turn(90);
+		    flash.move(90);
+		    flash.turn(90);
+		    flash.move(90);
+		    flash.turn(90);
+	}
+
+	private static void drawTriangle() {
+	flash.setPenColor(255,165,0);	
+		flash.penUp();
+		flash.turn(-90);
+		flash.move(90);
+		flash.penDown();
+		for (int i = 0; i < 3; i++) {
+			flash.move(120);
+			flash.turn(120);
+		}
+
+	}
+
+	private static void drawCircle() {
+		flash.setPenColor(255,215,0);
+		flash.penUp();
+		flash.move(90);
+		flash.move(90);
+		flash.move(90);
+		flash.penDown();
+		for (int i = 0; i < 36; i++) {
+			flash.move(10);
+			flash.turn(10);
+			
+		}
+		flash.penUp();
+		flash.turn(90);
+		flash.sparkle();
+	}
+	
+	
 }
