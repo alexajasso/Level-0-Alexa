af45e9501106484a59f67e84e5b10d8af6a1e1a7
alexajasso <league@Mac-mini-4.local>
2018-05-19 11:25:38 -0700

    Finished 1-5 working on 7.

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -2,22 +2,34 @@
 public class ForLoopGauntlet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Display all numbers from 0-100
 		// for(int i=0; i<=100; i++) {
 		// System.out.println(i);
+
+		// Display all numbers from 100-0
 		// for (int i = 100; i >= 0; i--) {
 		// System.out.println(i);
-		for (int i = 1; i <= 99; i++) {
 
-			if (i % 3 == 0) {
-				System.out.println(i);
-				
-	
-			}
+		// Display all even numbers 2-100
+		// for (int i = 2; i <= 100; i++) {
+		// if (i % 2 == 0) {
+		// System.out.println(i);
 
+		// Display all odd numbers 1-99
+		// for (int i = 1; i <= 99; i++) {
+		// if (i % 2 == 1) {
+		// System.out.println(i);
+
+		// Display all multiples of 7 from 0 to 777
+		//for (int i = 7; i <= 777; i = i + 7) {
+			//System.out.println(i);
 		}
+
+	//}
+
+	
+	{
+
 	}
 
-	{
-	}
 }
