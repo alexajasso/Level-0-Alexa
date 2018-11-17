2524856e587bc4cf7f5d401419c987a03cf3275d
Kendra Kleber <league@Mac-mini-2.local>
2018-11-17 11:24:50 -0800

    looking back at robot and doing for loop onslaught

--- src/ObedientRobot2.java
+++ src/ObedientRobot2.java
@@ -2,30 +2,31 @@ import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
-public class ObedientRobot {
+public class ObedientRobot2 {
+
 	static Robot flash = new Robot();
 
 	public static void main(String[] args) {
-		
+
 		flash.setSpeed(10);
 		int colorR = 0;
 		int colorG = 0;
 		int colorB = 0;
-		String color = JOptionPane.showInputDialog(null, "What color do you want red, orange, or yellow ?");
+		String color = JOptionPane.showInputDialog(null, "What color do you want Violet, Light Green, or Teal ?");
 		String shape = JOptionPane.showInputDialog(null, "What shape do you want circle, triangle, or square?");
 
-		if (color.equalsIgnoreCase("Red")) {
-			colorR = 255;
-			colorG = 0;
-			colorB = 0;
-		} else if (color.equalsIgnoreCase("orange")) {
-			colorR = 255;
-			colorG = 165;
-			colorB = 0;
+		if (color.equalsIgnoreCase("Violet")) {
+			colorR = 244;
+			colorG = 66;
+			colorB = 241;
+		} else if (color.equalsIgnoreCase("Light Green")) {
+			colorR = 66;
+			colorG = 244;
+			colorB = 125;
 		} else {
-			colorR = 255;
-			colorG = 215;
-			colorB = 0;
+			colorR = 66;
+			colorG = 244;
+			colorB = 206;
 		}
 
 		if (shape.equalsIgnoreCase("Square")) {
@@ -43,9 +44,9 @@ public class ObedientRobot {
 		flash.setPenColor(r, g, b);
 		flash.penDown();
 		for (int i = 0; i < 4; i++) {
-			
+
 			flash.move(90);
-			flash.turn(90);	
+			flash.turn(90);
 		}
 		flash.sparkle();
 	}
@@ -61,16 +62,14 @@ public class ObedientRobot {
 			flash.move(120);
 			flash.turn(120);
 		}
-			flash.sparkle();
-			
+		flash.sparkle();
 
 	}
 
 	private static void drawCircle(int r, int g, int b) {
 		flash.setPenWidth(50);
 		flash.setPenColor(r, g, b);
-		
-		
+
 		flash.penDown();
 		for (int i = 0; i < 36; i++) {
 			flash.move(10);
